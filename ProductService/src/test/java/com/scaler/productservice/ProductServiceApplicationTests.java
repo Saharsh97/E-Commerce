dedd4782bc4dfe5719e7b29d4f0b9b2a0cb59969
saharsh.s <saharsh.s@media.net>
2024-02-15 08:21:16 +0530

    added RandomTests with assertions

--- ProductService/src/test/java/com/scaler/productservice/ProductServiceApplicationTests.java
+++ ProductService/src/test/java/com/scaler/productservice/ProductServiceApplicationTests.java
@@ -17,79 +17,36 @@ import java.util.Optional;
 @SpringBootTest
 class ProductServiceApplicationTests {
 
-    ProductRepository productRepository;
-    CategoryRepository categoryRepository;
 
-    @Autowired
-    public ProductServiceApplicationTests(ProductRepository productRepository, CategoryRepository categoryRepository){
-        this.productRepository = productRepository;
-        this.categoryRepository = categoryRepository;
+    public ProductServiceApplicationTests(){
+
     }
+
+    // has the Spring context (container, which contains all the Spring beans),
+    // loaded properly or not, before taking any incoming request.
     @Test
     void contextLoads() {
-    }
 
-    @Test
-    void getData(){
-        List<Product> products = productRepository.something(52L);
-        for(Product p: products){
-            System.out.println(p.getId() + " " + p.getTitle());
-        }
-    }
-
-    @Test
-    void getSpecificData(){
-        List<ProductWithIdTitlePrice> products = productRepository.somethingSpecific(52L);
-        for(ProductWithIdTitlePrice p: products){
-            System.out.println(p.getId() + " " + p.getTitle());
-        }
-    }
-
-    @Test
-    void getDataUsingSQLQuery(){
-        List<Product> products = productRepository.somethingSQL(52L);
-        for(Product p: products){
-            System.out.println(p.getId() + " " + p.getTitle());
-        }
-    }
-
-    @Test
-    void getDataUsingSQLQueryFields(){
-        List<ProductWithIdTitlePrice> products = productRepository.somethingSQLWithFields(52L);
-        for(ProductWithIdTitlePrice p: products){
-            System.out.println(p.getId() + " " + p.getTitle());
-        }
-    }
-
-//    @Test
-//    @Transactional
-//    void getCategoryDetails(){
-//        Optional<Category> categoryOptional = categoryRepository.findById(2L);
-//        Category category = categoryOptional.get();
-//        System.out.println(category.getName());
-//        List<Product> products = category.getProducts();
-//        for(Product p: products){
-//            System.out.println(p.getId() + " : " + p.getPrice());
-//        }
-//    }
-    // what is faster?
-    // 2 DB calls OR 1 DB with JOIN?
-    // 2 DB calls will be slow.
-    // 2 DB calls in 5% of the cases, is better a JOIN in 100% of cases.
-
-    // lazy fetch
-    // in 95% scenarios, you only want to get category.name() -> 1 DB call without join.
-    // in only 5% scenarios, you want to get the category.name() + category.products() -> 2 DB calls.
-
-    // eager fetch.
-    // in 95% scenarios, you only want to get category.name() -> 1 DB call with join.
-    // you are wasting time taking join on the products, when it is not even needed!
-
-    // FetchMode. This is not relevant. because your ORM ignores FetchMode.
-    @Test
-    @Transactional
-    @Commit
-    void deleteEntries(){
-        productRepository.deleteById(300L);
     }
 }
+
+// you click on start Application
+// Spring will create a container.
+// Spring create the beans, and container stores all the beans
+// we have a bean called ProductServiceApplicationTests.
+// Spring ensures that all @Test methods run first,
+// if all successful, you take incoming request.
+// even if you dont write any testcase, you still have 1 test case : contextLoads()
+
+
+// if the applications gets any error while starting
+// then this contextLoads() will not run.
+
+// Spring ensures that all @Test methods run first,
+// but this testcase did not run.
+
+//something is wrong!
+// dont start the application.
+
+
+// break till 8:25
---
+++ ProductService/src/test/java/com/scaler/productservice/controllers/ProductControllerTest.java
@@ -0,0 +1,7 @@
+package com.scaler.productservice.controllers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductControllerTest {
+
+}
